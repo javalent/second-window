bcc7faf538a2516e4fc6d275792e78d63528bd9c
derammo <817160+derammo@users.noreply.github.com>
2023-03-03 16:46:46 -0500

    operational
    factored window and implemented default window
    in terms of the new class
    implemented additional windows

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,1 +1,29 @@
-export interface PluginSettings {}
+export interface PluginSettings {
+    saveWindowLocations: boolean;
+    /**
+     * indexed by configured window name, plus the default window at "DEFAULT"
+     */ 
+    windows?: Record<string, WindowSettings>;
+}
+
+export interface WindowSettings {
+    name: string;
+    /**
+     * not serialized
+     */
+    id?: number;
+
+    /**
+     * indexed by host name
+     */
+    hosts: Record<string, WindowState>;
+}
+
+export interface WindowState {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+    maximized: boolean;
+    fullscreen: boolean;
+}
